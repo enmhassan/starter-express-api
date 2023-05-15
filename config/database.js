810692c32a5bfae738832f2928dc4e285ecdad56
enmhassan <en.mhassan@gmail.com>
2023-05-15 11:36:20 +0300

    update fix

--- config/database.js
+++ config/database.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
-const express = require('express')
-const app = express();
+// const mongoose = require("mongoose");
+// const express = require('express')
+// const app = express();
 
-const { MONGO_URI } = process.env;
+// const { MONGO_URI } = process.env;
 
-exports.connect = () => {
-    //connecting to the database
-    mongoose.connect(
-        "mongodb+srv://enmhassan:fpVmF1VpgRzjB0yu@cluster0.ho0mqai.mongodb.net/?retryWrites=true&w=majority", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    )
-    .then(() => {
-        console.log("Successfully connected to database")
-        app.listen(port, ()=>{
-            console.log(`Server is listening on port ${port}`)
-        })
-    })
-    .catch((error) => {
-        console.log("database connection failed");
-        console.error(error);
-        process.exit(1);
-    });
-};
+// exports.connect = () => {
+//     //connecting to the database
+//     mongoose.connect(
+//         "mongodb+srv://enmhassan:fpVmF1VpgRzjB0yu@cluster0.ho0mqai.mongodb.net/?retryWrites=true&w=majority", {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         }
+//     )
+//     .then(() => {
+//         console.log("Successfully connected to database")
+//         app.listen(port, ()=>{
+//             console.log(`Server is listening on port ${port}`)
+//         })
+//     })
+//     .catch((error) => {
+//         console.log("database connection failed");
+//         console.error(error);
+//         process.exit(1);
+//     });
+// };
