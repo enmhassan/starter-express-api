bdbd00faa2c62da8e7a8837e9db9ad2e75008895
enmhassan <en.mhassan@gmail.com>
2023-05-25 14:42:08 +0300

    update

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -7,22 +7,13 @@ const verifyToken = (req, res, next) => {
         // || req.body.token || req.query.token || req.headers["x-access-token"];
     // console.log(token)
     if (!token) {
-        if (req.path !== '/login' && req.path !== '/register') {
-            return res.status(401).redirect('login')
-        } else {
-            return res.status(200)
-        }
+        return res.redirect('login')
     }
     try {
         const decoded = jwt.verify(token, config.TOKEN_KEY);
         req.user = decoded;
     } catch (err) {
-        if (req.path !== '/login' && req.path !== '/register') {
-            return res.status(401).redirect('login')
-        } else {
-            return res.status(200)
-        }
-
+        return res.status(401).redirect('login');
     }
     return next();
 };
