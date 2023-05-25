5d6a9490b50c124468b105b589726716eab55af0
enmhassan <en.mhassan@gmail.com>
2023-05-25 14:38:21 +0300

    update

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-    console.log(req)
     const token = req.cookies.token;
         // || req.body.token || req.query.token || req.headers["x-access-token"];
     // console.log(token)
